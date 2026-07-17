4b4dbbfdf66b775152a5f00eecd4abf4dcaf3694
Yevgeny Matveyev <yev_mtv@icloud.com>
2026-07-17 14:07:52 +0300

    Add pixel-art motorcycle favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script>
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import SiteHeader from '$lib/components/SiteHeader.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" sizes="64x64" href={favicon} />
 	<meta
 		name="description"
 		content="Двенадцатидневный маршрут на байках из Дананга (Đà Nẵng) через Центральное нагорье и обратно по побережью."
